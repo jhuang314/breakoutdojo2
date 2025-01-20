f85f1777349cf6f49ac6de05418a669685ef15da
jh <>
2025-01-19 23:12:04 -0500

    update frontend to use deployed katana and torii

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -4,4 +4,8 @@ import manifest from "../contract/manifest_dev.json";
 
 export const dojoConfig = createDojoConfig({
     manifest,
+    // Remove this to use local server for development.
+    rpcUrl: "https://api.cartridge.gg/x/breakoutdojo2/katana",
+    toriiUrl: "https://api.cartridge.gg/x/breakoutdojo2/torii",
+    relayUrl: "",
 });
